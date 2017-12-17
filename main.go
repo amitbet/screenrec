4a60a2a50bb29aaed506370a04ce2f46975ca596
amit bezalel <amit.bezalel@hpe.com>
2017-12-18 01:46:29 +0200

    refactoring and introducing a working mjpeg codec

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"image"
 	"image/color"
@@ -9,10 +10,14 @@ import (
 	"os"
 	"os/exec"
 	"screenrec/screenshot"
+	"strings"
+	"time"
 	"vnc2webm/logger"
+
+	"github.com/icza/mjpeg"
 )
 
-func encodePPM1(w io.Writer, m image.Image) error {
+func encodePPMFast(w io.Writer, m image.Image) error {
 	maxvalue := 255
 	b := m.Bounds()
 	// write header
@@ -43,7 +48,7 @@ func encodePPM1(w io.Writer, m image.Image) error {
 // Encode an image.Image as a PPM image and write the result to w. The resulting
 // image always has a MaxVal of 255. Alpha values are treated as if the image
 // was displayed on a white background.
-func EncodePPM(w io.Writer, m image.Image) error {
+func encodePPM(w io.Writer, m image.Image) error {
 	logger.Debug("start encode ppm")
 	bounds := m.Bounds().Canon()
 
@@ -98,21 +103,16 @@ func loadImage(path string) image.Image {
 	return img
 }
 
-func main() {
+type DV8ImageEncoder struct {
+	cmd   *exec.Cmd
+	input io.WriteCloser
+}
 
-	// if err != nil {
-	// 	// replace this with real error handling
-	// 	panic(err)
-	// }
-
-	const framerate int = 10
-	//cmd := exec.Command("./ffmpeg", "-f", "image2pipe", "-vcodec", "ppm", "-r", "20", "-i", "-", "-r", "10", "-c:v", "libx264", "-preset", "slow", "-crf", "22", "-c:a", "copy", "video.avi")
-	println("starting")
-	//////////////
-	// binary, lookErr := exec.LookPath("ffmpeg")
-	// if lookErr != nil {
-	// 	panic(lookErr)
-	// }
+func (enc *DV8ImageEncoder) Init(videoFileName string) {
+	fileExt := ".webm"
+	if !strings.HasSuffix(videoFileName, fileExt) {
+		videoFileName = videoFileName + fileExt
+	}
 	binary := "./ffmpeg"
 	cmd := exec.Command(binary,
 		"-f", "image2pipe",
@@ -145,76 +145,262 @@ func main() {
 		//"-slices", "4",
 		//"-vb", "2M",
 
-		"./stam.webm",
+		videoFileName,
 	)
 	//cmd := exec.Command("/bin/echo")
 
-	encInput, err := cmd.StdinPipe()
-	if err != nil {
-		logger.Error("can't get ffmpeg input pipe")
-	}
 	//io.Copy(cmd.Stdout, os.Stdout)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	//os.Stdout.Write(out)
-	//create screenshot frames and send to the ffmpeg process via stdin in ppm format
-	logger.Infof("launching screencap routine")
+
+	encInput, err := cmd.StdinPipe()
+	enc.input = encInput
+	if err != nil {
+		logger.Error("can't get ffmpeg input pipe")
+	}
+	enc.cmd = cmd
+}
+func (enc *DV8ImageEncoder) Run() {
+	logger.Debugf("launching binary: %v", enc.cmd.Args)
+	err := enc.cmd.Run()
+	if err != nil {
+		logger.Error("error while launching ffmpeg:", err)
+	}
+}
+func (enc *DV8ImageEncoder) Encode(img image.Image) {
+	err := encodePPMFast(enc.input, img)
+	if err != nil {
+		logger.Error("error while encoding image:", err)
+	}
+}
+func (enc *DV8ImageEncoder) Close() {
+
+}
+
+type X264ImageEncoder struct {
+	cmd   *exec.Cmd
+	input io.WriteCloser
+}
+
+func (enc *X264ImageEncoder) Init(videoFileName string) {
+	fileExt := ".mp4"
+	if !strings.HasSuffix(videoFileName, fileExt) {
+		videoFileName = videoFileName + fileExt
+	}
+	binary := "./ffmpeg"
+	cmd := exec.Command(binary,
+		"-f", "image2pipe",
+		"-vcodec", "ppm",
+		//"-r", strconv.Itoa(framerate),
+		"-r", "4",
+		//"-i", "pipe:0",
+		"-i", "-",
+		"-vcodec", "libx264", //"libvpx",//"libvpx-vp9"//"libx264"
+		"-b:v", "2M",
+		"-threads", "8",
+		//"-speed", "0",
+		//"-lossless", "1", //for vpx
+		// "-tile-columns", "6",
+		//"-frame-parallel", "1",
+		// "-an", "-f", "webm",
+		"-cpu-used", "-16",
+
+		"-preset", "ultrafast",
+		"-deadline", "realtime",
+		//"-cpu-used", "-5",
+		"-maxrate", "2.5M",
+		"-bufsize", "10M",
+		"-g", "6",
+
+		//"-rc_lookahead", "16",
+		//"-profile", "0",
+		"-qmax", "51",
+		"-qmin", "11",
+		//"-slices", "4",
+		//"-vb", "2M",
+
+		videoFileName,
+	)
+	//cmd := exec.Command("/bin/echo")
+
+	//io.Copy(cmd.Stdout, os.Stdout)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	encInput, err := cmd.StdinPipe()
+	enc.input = encInput
+	if err != nil {
+		logger.Error("can't get ffmpeg input pipe")
+	}
+	enc.cmd = cmd
+}
+func (enc *X264ImageEncoder) Run() {
+	logger.Debugf("launching binary: %v", enc.cmd.Args)
+	err := enc.cmd.Run()
+	if err != nil {
+		logger.Error("error while launching ffmpeg:", err)
+	}
+}
+func (enc *X264ImageEncoder) Encode(img image.Image) {
+	err := encodePPMFast(enc.input, img)
+	if err != nil {
+		logger.Error("error while encoding image:", err)
+	}
+}
+func (enc *X264ImageEncoder) Close() {
+
+}
+
+type DV9ImageEncoder struct {
+	cmd   *exec.Cmd
+	input io.WriteCloser
+}
+
+func (enc *DV9ImageEncoder) Init(videoFileName string) {
+	fileExt := ".webm"
+	if !strings.HasSuffix(videoFileName, fileExt) {
+		videoFileName = videoFileName + fileExt
+	}
+	binary := "./ffmpeg"
+	cmd := exec.Command(binary,
+		"-f", "image2pipe",
+		"-vcodec", "ppm",
+		//"-r", strconv.Itoa(framerate),
+		"-r", "3",
+		//"-i", "pipe:0",
+		"-i", "-",
+		"-vcodec", "libvpx-vp9", //"libvpx",//"libvpx-vp9"//"libx264"
+		"-b:v", "2M",
+		"-threads", "8",
+		//"-speed", "0",
+		//"-lossless", "1", //for vpx
+		// "-tile-columns", "6",
+		//"-frame-parallel", "1",
+		// "-an", "-f", "webm",
+		"-cpu-used", "-16",
+
+		"-preset", "ultrafast",
+		"-deadline", "realtime",
+		//"-cpu-used", "-5",
+		"-maxrate", "2.5M",
+		"-bufsize", "10M",
+		"-g", "6",
+
+		//"-rc_lookahead", "16",
+		//"-profile", "0",
+		"-qmax", "51",
+		"-qmin", "11",
+		//"-slices", "4",
+		//"-vb", "2M",
+
+		videoFileName,
+	)
+	//cmd := exec.Command("/bin/echo")
+
+	//io.Copy(cmd.Stdout, os.Stdout)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	encInput, err := cmd.StdinPipe()
+	enc.input = encInput
+	if err != nil {
+		logger.Error("can't get ffmpeg input pipe")
+	}
+	enc.cmd = cmd
+}
+func (enc *DV9ImageEncoder) Run() {
+	logger.Debugf("launching binary: %v", enc.cmd.Args)
+	err := enc.cmd.Run()
+	if err != nil {
+		logger.Error("error while launching ffmpeg:", err)
+	}
+}
+func (enc *DV9ImageEncoder) Encode(img image.Image) {
+	err := encodePPMFast(enc.input, img)
+	if err != nil {
+		logger.Error("error while encoding image:", err)
+	}
+}
+func (enc *DV9ImageEncoder) Close() {
+
+}
+
+type MJPegImageEncoder struct {
+	avWriter  mjpeg.AviWriter
+	Quality   int
+	Framerate int32
+}
+
+func (enc *MJPegImageEncoder) Init(videoFileName string) {
+	fileExt := ".avi"
+	if !strings.HasSuffix(videoFileName, fileExt) {
+		videoFileName = videoFileName + fileExt
+	}
+	if enc.Framerate <= 0 {
+		enc.Framerate = 5
+	}
+	avWriter, err := mjpeg.New(videoFileName, 1024, 768, enc.Framerate)
+	if err != nil {
+		logger.Error("Error during mjpeg init: ", err)
+	}
+	enc.avWriter = avWriter
+}
+func (enc *MJPegImageEncoder) Run() {
+}
+func (enc *MJPegImageEncoder) Encode(img image.Image) {
+	buf := &bytes.Buffer{}
+	jOpts := &jpeg.Options{Quality: enc.Quality}
+	if enc.Quality <= 0 {
+		jOpts = nil
+	}
+	err := jpeg.Encode(buf, img, jOpts)
+	if err != nil {
+		logger.Error("Error while creating jpeg: ", err)
+	}
+	err = enc.avWriter.AddFrame(buf.Bytes())
+	if err != nil {
+		logger.Error("Error while adding frame to mjpeg: ", err)
+	}
+
+}
+func (enc *MJPegImageEncoder) Close() {
+	err := enc.avWriter.Close()
+	if err != nil {
+		logger.Error("Error while closing mjpeg: ", err)
+	}
+}
+
+type ImageEncoder interface {
+	Init(string)
+	Run()
+	Encode(image.Image)
+	Close()
+}
+
+func runCaptureLoop(videoFileName string, codec ImageEncoder) {
+
+	codec.Init(videoFileName)
 	go func() {
-		// infile, err := os.Open("./img.jpg")
-		// if err != nil {
-		// 	panic(err)
-		// }
-		// defer infile.Close()
-		// myImg, _ := jpeg.Decode(infile)
-
 		for {
-			logger.Infof(">>>inside screencap routine")
-			//defer encInput.Close()
-			//----------------------
-
 			img, err := screenshot.CaptureScreen()
-
 			if err != nil {
 				println("err %v", err)
 			}
 			myImg := image.Image(img)
-			//-----------------------
 
 			logger.Infof(">>sending screen in ppm, size: %v", myImg.Bounds().Max)
-			//png.Encode(encInput, myImg)
-			encodePPM1(encInput, myImg)
-
-			// mjenc := NewMJpegEncoder("./stam")
-			// mjenc.EncodeFrame(myImg)
-			if err != nil {
-				logger.Errorf("err while writing ppm to ffmpeg %v", err)
-			}
-			//time.Sleep(time.Duration(1000/framerate) * time.Millisecond)
+			codec.Encode(myImg)
 		}
 
 	}()
+	codec.Run()
+	time.Sleep(30 * time.Second)
+	codec.Close()
+}
 
-	logger.Debugf("launching binary: %v", cmd.Args)
-	err = cmd.Run()
-	//time.Sleep(1* time.Second)
-	if err != nil {
-		logger.Error(err)
-	}
-	//cmd.Wait()
+func main() {
 
-	/////////////
-
-	// encOutput, err := cmd.StdoutPipe()
-	// if err != nil {
-	// 	logger.Errorf("can't get ffmpeg output pipe %v", err)
-	// }
-
-	// //defer encInput.Close()
-	// err = cmd.Run()
-	// if err != nil {
-	// 	logger.Error("error while running ffmpeg", err)
-	// }
-
-	// io.Copy(os.Stdout, encOutput)
-	//cmd.Wait()
+	//enc := &MJPegImageEncoder{Quality: 60, Framerate: 6}
+	enc := &X264ImageEncoder{}
+	runCaptureLoop("stam", enc)
 }
